7d5f7e6c19c25b0e073307e917ad01d0f08d66c1
Harish <harish.kirubaharan@ontariotechu.net>
2023-03-09 20:20:11 -0500

    dev

--- spamDetectorServer/src/main/java/com/spamdetector/util/SpamDetector.java
+++ spamDetectorServer/src/main/java/com/spamdetector/util/SpamDetector.java
@@ -5,7 +5,6 @@ import com.spamdetector.domain.TestFile;
 import java.io.*;
 import java.util.*;
 
-
 /**
  * TODO: This class will be implemented by you
  * You may create more methods to help you organize you strategy and make you code more readable
@@ -21,52 +20,31 @@ public class SpamDetector {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        HashMap<String, Integer> spamWordCount = new HashMap<String, Integer>();
-        HashMap<String, Integer> hamWordCount = new HashMap<String, Integer>();
+        Map<String, Integer> spamWordCount = new HashMap<>();
 
         File spamFile = new File("csci2020u-assignment01\\spamDetectorServer\\src\\main\\resources\\data\\train\\spam");
         File hamFile = new File("csci2020u-assignment01\\spamDetectorServer\\src\\main\\resources\\data\\train\\ham");
         File ham2File = new File("csci2020u-assignment01\\spamDetectorServer\\src\\main\\resources\\data\\train\\ham2");
 
-//        File[] spamFiles = spamFile.listFiles();
-//        File[] hamFiles = hamFile.listFiles();
-//        File[] ham2Files = ham2File.listFiles();
-//
-//        System.out.println("Spam Files: " + spamFiles.length + " Ham Files: " + hamFiles.length + " Ham2 Files: " + ham2Files.length);
-
-        for(int i = 0; i < spamFile.listFiles().length; i++) {
-            try {
-                BufferedReader reader = new BufferedReader(new FileReader(Objects.requireNonNull(spamFile.listFiles())[i]));
-                String line = reader.readLine();
-                String[] words;
-                System.out.println(spamFile.listFiles()[i].getName() + " IM HERE KEKW");
-
-                while((reader.readLine()) != null) {
-                    words = line.split(" ");
-                    int counter = 0;
-                    for (String word : words) {
-
-
-
+        for (File file : spamFile.listFiles()) {
+            try (Scanner scanner = new Scanner(file)) {
+                Set<String> wordsSeen = new HashSet<>();
+                while (scanner.hasNext()) {
+                    String word = scanner.next().toLowerCase();
+                    if (!wordsSeen.contains(word)) {
+                        wordsSeen.add(word);
                         if (spamWordCount.containsKey(word)) {
                             spamWordCount.put(word, spamWordCount.get(word) + 1);
                         } else {
                             spamWordCount.put(word, 1);
                         }
-
-
                     }
-                    line = reader.readLine();
                 }
-            }catch (Exception e) {
-                System.out.println("Error >> " + e);
             }
         }
-        System.out.println(spamWordCount.size());
 
-        System.out.println();
+        System.out.println(spamWordCount.get("emails"));
 
     }
 
 }
-
