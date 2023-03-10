bd8b22820267f8d4ad19992fd7df3a6cddbb2df3
Harish <harish.kirubaharan@ontariotechu.net>
2023-03-09 20:44:06 -0500

    dev

--- spamDetectorServer/src/main/java/com/spamdetector/util/SpamDetector.java
+++ spamDetectorServer/src/main/java/com/spamdetector/util/SpamDetector.java
@@ -21,6 +21,8 @@ public class SpamDetector {
     public static void main(String[] args) throws FileNotFoundException {
 
         Map<String, Integer> spamWordCount = new HashMap<>();
+        Map<String, Integer> hamCount = new HashMap<>();
+        Set<String> stopwords = getStopwords();
 
         File spamFile = new File("csci2020u-assignment01\\spamDetectorServer\\src\\main\\resources\\data\\train\\spam");
         File hamFile = new File("csci2020u-assignment01\\spamDetectorServer\\src\\main\\resources\\data\\train\\ham");
@@ -31,7 +33,7 @@ public class SpamDetector {
                 Set<String> wordsSeen = new HashSet<>();
                 while (scanner.hasNext()) {
                     String word = scanner.next().toLowerCase();
-                    if (!wordsSeen.contains(word)) {
+                    if (!stopwords.contains(word) && !wordsSeen.contains(word)) {
                         wordsSeen.add(word);
                         if (spamWordCount.containsKey(word)) {
                             spamWordCount.put(word, spamWordCount.get(word) + 1);
@@ -43,8 +45,57 @@ public class SpamDetector {
             }
         }
 
-        System.out.println(spamWordCount.get("emails"));
+        for (File file : hamFile.listFiles()) {
+            try (Scanner scanner = new Scanner(file)) {
+                Set<String> wordsSeen = new HashSet<>();
+                while (scanner.hasNext()) {
+                    String word = scanner.next().toLowerCase();
+                    if (!stopwords.contains(word) && !wordsSeen.contains(word)) {
+                        wordsSeen.add(word);
+                        if (hamCount.containsKey(word)) {
+                            hamCount.put(word, hamCount.get(word) + 1);
+                        } else {
+                            hamCount.put(word, 1);
+                        }
+                    }
+                }
+            }
+        }
 
+        System.out.println(spamWordCount.get("wrinkles"));
+
+        //Print out the top 10 most common words in spam
+        List<Map.Entry<String, Integer>> spam = new ArrayList<>(spamWordCount.entrySet());
+        spam.sort(Map.Entry.comparingByValue());
+        Collections.reverse(spam);
+        System.out.println("Top 10 most common words in spam:");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(spam.get(i));
+        }
+
+        List<Map.Entry<String, Integer>> ham = new ArrayList<>(hamCount.entrySet());
+        ham.sort(Map.Entry.comparingByValue());
+        Collections.reverse(ham);
+        System.out.println("Top 10 most common words in ham:");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(ham.get(i));
+        }
+
+
+
+    }
+
+    private static Set<String> getStopwords() {
+
+        Set<String> stopwords = new HashSet<>();
+        try (Scanner scanner = new Scanner(new File("E:\\Desktop\\assspam\\csci2020u-assignment01\\spamDetectorServer\\src\\main\\resources\\stopword.txt"))) {
+            while (scanner.hasNext()) {
+                stopwords.add(scanner.next().toLowerCase());
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Stopword list not found");
+        }
+        return stopwords;
     }
 
 }
